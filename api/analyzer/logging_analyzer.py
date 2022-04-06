6427a9b16964dcf172b71b310236255f3b6ae874
Mårten Björkman <marten.bjorkman@apelkado.com>
2022-04-06 22:01:00 +0200

    Added logging for analyser.

---
+++ api/analyzer/logging_analyzer.py
@@ -0,0 +1,9 @@
+import os
+import logging
+import sys
+
+logging.basicConfig(
+    level=os.environ.get('LOGLEVEL', 'WARNING').upper(),
+    datefmt='%Y-%m-%d %H:%M:%S',
+    format='%(asctime)s %(levelname)-8s %(message)s')
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))

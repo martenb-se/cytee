b9e23a4e32fdab925e4adebf35fd032dd9b650eb
Mårten Björkman <marten.bjorkman@apelkado.com>
2022-04-12 08:00:20 +0200

    Updated tests to work with redesigned analyzer.

--- api/tests/analyzer/test_analyzer.py
+++ api/tests/analyzer/test_analyzer.py
@@ -1,24 +1,47 @@
 import base64
 import pytest
-from api.analyzer.analyzer import analyze_files, EsprimaAnalyze
+from api.analyzer.analyzer import analyze_files, AnalyzeJS
 from api.tests.analyzer.mock_data_test_analyzer import CONTENTS_FILE_jira_clone_client_src_shared_utils_api, \
     CONTENTS_FILE_jira_clone_client_src_shared_utils_authToken, CONTENTS_FILE_projectgroup2_src_external_dataPersist
 
 
 @pytest.fixture()
 def mocker_open(mocker):
+    """Pytest mocker for built in function: open(filename, operation)
+    :param mocker: Pytest mocker.
+    :return: The custom mocker.
+    """
+
     class MockOpen:
         def __init__(self, filename, operation):
+            """Mock replacement for build in function open(filename, operation)
+            :param filename: The filename (with filepath) to open.
+            :type filename: str
+            :param operation: The operation code to read the file with. This option is of no importance to this mocker
+            and its contents will be ignored.
+            :type filename: str
+            """
             self.filename = filename
             self.operation = operation
             self.status = "open"
 
-        # Used for: "with open(...):"
         def __enter__(self):
+            """Returns the own class instance.
+            Note: Method is called when running "with open(...):" and therefore must be defined, it's effect is of
+            no importance to the mocker.
+            :return: The class instance.
+            """
             return self
 
-        # Used for: "with open(...):"
         def __exit__(self, arg1, arg2, arg3):
+            """Sets the internal file status to "closed".
+            Note: Method is called when running "with open(...):" and therefore must be defined, it's effect is of
+            no importance to the mocker.
+            :param arg1: Positional argument 1 is of no importance to the mocker.
+            :param arg2: Positional argument 2 is of no importance to the mocker.
+            :param arg3: Positional argument 3 is of no importance to the mocker.
+            :return: Nothing
+            """
             self.status = "closed"
 
         def read(self):
@@ -36,65 +59,60 @@ def mocker_open(mocker):
         def close(self):
             self.status = "closed"
 
-    # Function to pass to patcher
     def patcher_open(filename, operation):
+        """The patcher function to use instead of open().
+        :param filename: The filename (with filepath) to open.
+        :type filename: str
+        :param operation: The operation code to read the file with. This option is of no importance to this mocker and
+        its contents will be ignored.
+        :type filename: str
+        :return: The initiated mocker instance.
+        """
         return MockOpen(filename, operation)
 
-    mock_open = mocker.patch('api.analyzer.analyzer.open', side_effect=patcher_open)
-    # yield mock_open
+    def mocker_return(to_mock):
+        """Mocker to send back to test.
+        :param to_mock: Variant of open() to mock.
+        :type to_mock: str
+        :return: Nothing
+        """
+        mocker.patch(to_mock, side_effect=patcher_open)
+
+    yield mocker_return
 
 
-def test_class_esprimaanalyze_init_not_string():
-    file_source = False
+def test_class_esprimaanalyze_init_file_location_not_string():
+    file_location = False
     try:
-        EsprimaAnalyze(file_source)
+        AnalyzeJS(file_location)
         assert False
     except TypeError as e:
-        assert str(e) == "'file_source' must be a STRING"
+        assert str(e) == "'file_location' must be a STRING"
     except Exception:
         assert False
 
 
-def test_class_esprimaanalyze_init_no_content():
-    file_source = ""
+def test_class_esprimaanalyze_init_file_location_empty():
+    file_location = ""
     try:
-        EsprimaAnalyze(file_source)
+        AnalyzeJS(file_location)
         assert False
     except ValueError as e:
-        assert str(e) == "'file_source' cannot be empty"
+        assert str(e) == "'file_location' cannot be empty"
     except Exception:
         assert False
 
 
-# TODO: Use test..
-def test_class_esprimaanalyze_process_functions_fun_a():
-    file_source = """const defaults = {
-  baseURL: process.env.API_URL || 'http://localhost:3000',
-  headers: () => ({
-    'Content-Type': 'application/json',
-    Authorization: getStoredAuthToken() ? `Bearer ${getStoredAuthToken()}` : undefined,
-  }),
-  error: {
-    code: 'INTERNAL_ERROR',
-    message: 'Something went wrong. Please check your internet connection or contact our support.',
-    status: 503,
-    data: {},
-  },
-  stuff: {
-      cookie: () => ({
-          stuff: '123'
-      }),
-      dragon: {
-          whoops: () => ({
-              thisstuff: '333'
-          })
-      }
-  }
-};"""
-
-    """source_handler = HandleEsprima(file_source)
-    source_handler.process_functions()"""
-    assert True
+def test_class_esprimaanalyze_init_project_root_not_string():
+    file_source = "/some/path/to/file.js"
+    project_root = False
+    try:
+        AnalyzeJS(file_source, project_root)
+        assert False
+    except TypeError as e:
+        assert str(e) == "'project_root' must be a STRING"
+    except Exception:
+        assert False
 
 
 def test_analyze_files_arg_not_list():
@@ -145,18 +163,3 @@ def test_analyze_files_arg_list_item_empty_string():
         assert str(e) == "Paths in 'list_of_files' cannot be empty strings"
     except Exception:
         assert False
-
-
-# TODO: Continue with testing
-"""
-def test_debug(mocker_open):
-    list_of_files = [
-        "/jira_clone/client/src/shared/utils/api.js",
-        "/jira_clone/client/src/shared/utils/authToken.js",
-        "/project-group-2/src/external/dataPersist.js"
-    ]
-
-    analyze_files(list_of_files)
-
-    assert False
-"""

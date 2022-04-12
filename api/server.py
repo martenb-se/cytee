e0166c3a34905b88a4fdcd4789bb2347354cf32b
Jonathan Bergqvist <jonathan.bergqvist.jb@gmail.com>
2022-04-12 14:55:49 +0200

    added list files without tests

--- api/server.py
+++ api/server.py
@@ -1,5 +1,7 @@
 import time
-from flask import Flask
+from api.api import *
+from flask import Flask, request, jsonify
+from pprint import pprint
 
 server = Flask(__name__, static_folder='../build', static_url_path='/')
 
@@ -7,6 +9,13 @@ server = Flask(__name__, static_folder='../build', static_url_path='/')
 @server.route('/')
 def index():
 	return "Hello, World!"
+
+
+@server.route('/api/list_files', methods=['POST'])
+def post_list_files():
+	content = request.json
+	file_list = list_files(content['sub_dir'])
+	return jsonify(file_list)
 
 
 @server.route('/api/time')

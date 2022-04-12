d82088cd1585208879e9d3f117e3779d3f4e6afb
Mårten Björkman <marten.bjorkman@apelkado.com>
2022-04-12 23:22:54 +0200

    Moved mocker for open away from file.

--- api/tests/analyzer/test_analyzer.py
+++ api/tests/analyzer/test_analyzer.py
@@ -1,84 +1,4 @@
-import base64
-import pytest
 from api.analyzer.analyzer import analyze_files, AnalyzeJS
-from api.tests.analyzer.mock_data_test_analyzer import CONTENTS_FILE_jira_clone_client_src_shared_utils_api, \
-    CONTENTS_FILE_jira_clone_client_src_shared_utils_authToken, CONTENTS_FILE_projectgroup2_src_external_dataPersist
-
-
-@pytest.fixture()
-def mocker_open(mocker):
-    """Pytest mocker for built in function: open(filename, operation)
-    :param mocker: Pytest mocker.
-    :return: The custom mocker.
-    """
-
-    class MockOpen:
-        def __init__(self, filename, operation):
-            """Mock replacement for build in function open(filename, operation)
-            :param filename: The filename (with filepath) to open.
-            :type filename: str
-            :param operation: The operation code to read the file with. This option is of no importance to this mocker
-            and its contents will be ignored.
-            :type filename: str
-            """
-            self.filename = filename
-            self.operation = operation
-            self.status = "open"
-
-        def __enter__(self):
-            """Returns the own class instance.
-            Note: Method is called when running "with open(...):" and therefore must be defined, it's effect is of
-            no importance to the mocker.
-            :return: The class instance.
-            """
-            return self
-
-        def __exit__(self, arg1, arg2, arg3):
-            """Sets the internal file status to "closed".
-            Note: Method is called when running "with open(...):" and therefore must be defined, it's effect is of
-            no importance to the mocker.
-            :param arg1: Positional argument 1 is of no importance to the mocker.
-            :param arg2: Positional argument 2 is of no importance to the mocker.
-            :param arg3: Positional argument 3 is of no importance to the mocker.
-            :return: Nothing
-            """
-            self.status = "closed"
-
-        def read(self):
-            file_contents = ""
-            if self.filename == "/jira_clone/client/src/shared/utils/api.js":
-                file_contents = base64.b64decode(CONTENTS_FILE_jira_clone_client_src_shared_utils_api).decode("utf-8")
-            elif self.filename == "/jira_clone/client/src/shared/utils/authToken.js":
-                file_contents = base64.\
-                    b64decode(CONTENTS_FILE_jira_clone_client_src_shared_utils_authToken).decode("utf-8")
-            elif self.filename == "/project-group-2/src/external/dataPersist.js":
-                file_contents = base64.b64decode(CONTENTS_FILE_projectgroup2_src_external_dataPersist).decode("utf-8")
-
-            return file_contents
-
-        def close(self):
-            self.status = "closed"
-
-    def patcher_open(filename, operation):
-        """The patcher function to use instead of open().
-        :param filename: The filename (with filepath) to open.
-        :type filename: str
-        :param operation: The operation code to read the file with. This option is of no importance to this mocker and
-        its contents will be ignored.
-        :type filename: str
-        :return: The initiated mocker instance.
-        """
-        return MockOpen(filename, operation)
-
-    def mocker_return(to_mock):
-        """Mocker to send back to test.
-        :param to_mock: Variant of open() to mock.
-        :type to_mock: str
-        :return: Nothing
-        """
-        mocker.patch(to_mock, side_effect=patcher_open)
-
-    yield mocker_return
 
 
 def test_class_esprimaanalyze_init_file_location_not_string():
@@ -146,7 +66,8 @@ def test_analyze_files_arg_list_item_not_string():
         analyze_files(list_of_files)
         assert False
     except ValueError as e:
-        assert str(e) == "'list_of_files' must only contain paths to files as STRINGS"
+        assert str(e) == \
+               "'list_of_files' must only contain paths to files as STRINGS"
     except Exception:
         assert False
 

1e46d942fed16779f81a6d1c94e875d1d60689aa
Mårten Björkman <marten.bjorkman@apelkado.com>
2022-04-15 20:41:29 +0200

    Updated logger to enable extra output if env LOGGER_STDOUT is set.

--- api/analyzer/logging_analyzer.py
+++ api/analyzer/logging_analyzer.py
@@ -2,7 +2,13 @@ import os
 import logging
 import sys
 
+LOGLEVEL = os.environ.get('LOGLEVEL', 'WARNING').upper()
+LOGGER_STDOUT = os.environ.get('LOGGER_STDOUT', '0').upper()
+
 logging.basicConfig(
-    level=os.environ.get('LOGLEVEL', 'WARNING').upper(),
+    level=LOGLEVEL,
     datefmt='%Y-%m-%d %H:%M:%S',
     format='%(asctime)s %(levelname)-8s %(message)s')
+
+if LOGGER_STDOUT == "1":
+    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))

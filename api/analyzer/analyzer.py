c07e78e0320f458f06de4037d247209fbe256e35
Mårten Björkman <marten.bjorkman@apelkado.com>
2022-04-20 08:35:15 +0200

    Separated WebSocket constants and added support for new database connectivity.

--- api/analyzer/analyzer.py
+++ api/analyzer/analyzer.py
@@ -4,15 +4,8 @@ import esprima
 import re
 from api.analyzer.logging_analyzer import logging
 from api.cache import save_file
-from api.database import database_handler
-
-# TODO: Move all of the below to a common file to be imported where it's
-#  needed.
-WEBSOCKET_STATUS_URL = "sock_choose_files"
-WEBSOCKET_ERR_FILE_EMPTY = "ERR_FILE_EMPTY"
-WEBSOCKET_ERR_FILE_MISSING = "ERR_FILE_MISSING"
-WEBSOCKET_ERR_PARSE_FAILURE = "ERR_PARSE_FAILURE"
-WEBSOCKET_ERR_UNEXPECTED = "ERR_UNEXPECTED"
+from api.instances.database_main import database_handler
+from api.websocket_constants import *
 
 
 class AnalyzeJS:

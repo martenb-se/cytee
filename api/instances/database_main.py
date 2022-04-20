d1754ba267865fe69f9b3743a4c5779e8f1ee451
Jonathan Bergqvist <jonathan.bergqvist.jb@gmail.com>
2022-04-20 09:05:19 +0200

    removed test condition from database_main

--- api/instances/database_main.py
+++ api/instances/database_main.py
@@ -4,7 +4,6 @@ import sys
 if __name__ != '__main__':
     try:
         database_handler = DatabaseHandler('mongodb://127.0.0.1:27017')
-        if hasattr(sys, '_called_from_test'):
-            database_handler.connect_to_db()
+        database_handler.connect_to_db()
     except RuntimeError:
         pass

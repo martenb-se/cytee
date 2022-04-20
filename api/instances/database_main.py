d62e61da1e8ccbc861751d453778dc8c4ae64b87
Jonathan Bergqvist <jonathan.bergqvist.jb@gmail.com>
2022-04-20 08:53:03 +0200

    prevents connection to db when testing

--- api/instances/database_main.py
+++ api/instances/database_main.py
@@ -1,8 +1,10 @@
 from api.database import *
+import sys
 
 if __name__ != '__main__':
     try:
         database_handler = DatabaseHandler('mongodb://127.0.0.1:27017')
-        database_handler.connect_to_db()
+        if hasattr(sys, '_called_from_test'):
+            database_handler.connect_to_db()
     except RuntimeError:
         pass

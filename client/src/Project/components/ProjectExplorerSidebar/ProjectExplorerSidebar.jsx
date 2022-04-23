5e8c658d99b53ef4c37ef01f8641ed824bfd2f2c
Jonathan Bergqvist <jonathan.bergqvist.jb@gmail.com>
2022-04-23 19:29:57 +0200

    Initial setup for the test creator page

---
+++ client/src/Project/components/ProjectExplorerSidebar/ProjectExplorerSidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProjectExplorerSidebar(){
+    return (
+        <>
+            <div>ProjectExplorerSidebar</div>
+        </>
+    )
+}
+
+export default ProjectExplorerSidebar;

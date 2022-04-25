4fdfb8145c0799fed0aca7943c2b4de36030ef3e
Mårten Björkman <marten.bjorkman@apelkado.com>
2022-04-25 08:15:30 +0200

    Server now uses updated API. Interface changes for sockets.

--- api/server.py
+++ api/server.py
@@ -33,26 +33,145 @@ def post_list_files():
     """List files in the standard directory or from the specified sub
     directory.
 
-    :return: The file list in JSON.
+    :return: JSON with status code and a list of files if successful.
     """
     content = request.json
 
-    api_return = list_files(content['sub_dir'])
+    if 'subDirectory' in content:
+        api_return = list_files(content['subDirectory'])
+
+    else:
+        api_return = {
+            "status": APIStatus.ERROR.value,
+            "statusCode": APICode.ERROR_BAD_REQUEST.value,
+            "message":
+                "Required property 'subDirectory' is missing from request!"
+        }
 
     return jsonify(api_return)
 
 
-@server.route('/api/choose_files', methods=['POST'])
-def post_choose_files():
-    """Chose the selected files and begin the process of analyzing the files.
+@server.route('/api/new_project', methods=['POST'])
+def post_new_project():
+    """Create a new project from the specified path.
 
-    :return: The number of files to analyze in JSON.
+    :return: JSON with status code.
     """
     content = request.json
     path_to_project = content["pathToProject"]
-    list_of_files = content["chosenFiles"]
 
-    api_return = choose_files(path_to_project, list_of_files)
+    api_return = new_project(path_to_project)
+
+    return jsonify(api_return)
+
+
+@server.route('/api/choose_project', methods=['POST'])
+def post_choose_project():
+    """Choose and reopen an existing project.
+
+    :return: JSON with status code.
+    """
+    content = request.json
+    path_to_project = content["pathToProject"]
+
+    api_return = choose_project(path_to_project)
+
+    return jsonify(api_return)
+
+
+@server.route('/api/get_functions_for_project', methods=['POST'])
+def post_get_functions_for_project():
+    """Get all functions created for a project.
+
+    :return: JSON with status code.
+    """
+    content = request.json
+    path_to_project = content["pathToProject"]
+
+    api_return = get_functions_for_project(path_to_project)
+
+    return jsonify(api_return)
+
+
+@server.route('/api/read_file', methods=['POST'])
+def post_read_file():
+    """Read a project file.
+
+    :return: JSON with status code.
+    """
+    content = request.json
+    path_to_project = content["pathToProject"]
+    file_id = content["fileId"]
+
+    api_return = read_file(path_to_project, file_id)
+
+    return jsonify(api_return)
+
+
+@server.route('/api/get_tests_for_project', methods=['POST'])
+def post_get_tests_for_project():
+    """Get all tests for the given project.
+
+    :return: JSON with status code.
+    """
+    content = request.json
+    path_to_project = content["pathToProject"]
+
+    api_return = get_tests_for_project(path_to_project)
+
+    return jsonify(api_return)
+
+
+@server.route('/api/save_test', methods=['POST'])
+def post_save_test():
+    """Save a test for the given function.
+
+    :return: JSON with status code.
+    """
+    content = request.json
+    path_to_project = content["pathToProject"]
+    file_id = content["fileId"]
+    function_id = content["functionId"]
+    test_module = content["testModule"]
+
+    if 'customName' in content:
+        custom_name = content["customName"]
+    else:
+        custom_name = ""
+
+    api_return = \
+        save_test(
+            path_to_project, file_id, function_id, test_module, custom_name)
+
+    return jsonify(api_return)
+
+
+@server.route('/api/edit_test', methods=['POST'])
+def post_edit_test():
+    """Edit a test given by the specified Id.
+
+    :return: JSON with status code.
+    """
+    content = request.json
+    test_id = content["testId"]
+    test_module = content["testModule"]
+    custom_name = content["customName"]
+
+    api_return = edit_test(test_id, test_module, custom_name)
+
+    return jsonify(api_return)
+
+
+@server.route('/api/delete_test', methods=['POST'])
+def post_delete_test():
+    """Delete a test given by the specified Id.
+
+    :return: JSON with status code.
+    """
+    content = request.json
+    test_id = content["testId"]
+
+    api_return = delete_test(test_id)
 
     return jsonify(api_return)
 
@@ -68,7 +187,7 @@ def get_current_time():
     return {'time': time.time()}
 
 
-@socket.route('/sock_list_files')
+@socket.route('/sock/list_files')
 def sock_list_files(sock):
     """WebSocket for 'list_files' api. Listeners to this sockets will know
     when the listing of files is done.
@@ -83,18 +202,13 @@ def sock_list_files(sock):
     shared_websockets_handler.keep_alive(sock)
 
 
-@socket.route('/sock_choose_files')
-def sock_choose_files(sock):
+@socket.route('/sock/new_project')
+def sock_new_project(sock):
     """WebSocket for 'choose_files' api. Listeners to this sockets will know
     about events during the analysis of the selected files.
 
     :param sock: The instantiated socket to communicate over.
     :return: Nothing
     """
-    shared_websockets_handler.add_socket(WsIdentity.CHOOSE_FILES, sock)
+    shared_websockets_handler.add_socket(WsIdentity.NEW_PROJECT, sock)
     shared_websockets_handler.keep_alive(sock)
-
-    """while True:
-        # TODO: Should not act as an echo server later..
-        data = sock.receive()
-        sock.send(data)"""

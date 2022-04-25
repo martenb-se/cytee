3f947bdd2940891f574143c6725cd55580df367d
Mårten Björkman <marten.bjorkman@apelkado.com>
2022-04-25 09:59:28 +0200

    Added store.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import * as ReactDOMClient from 'react-dom/client';
+import {Provider} from 'react-redux';
+import store from 'reducers/store'
 
 import App from "./App";
 
 const container = document.getElementById('app');
 const root = ReactDOMClient.createRoot(container);
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);

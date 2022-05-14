5ae0ecb3d67335ea40257cac44e53baaf420c432
Jonathan Bergqvist <jonathan.bergqvist.jb@gmail.com>
2022-05-14 19:23:42 +0200

    Implemented ability to choose 'not equal' as an option for return values and exceptions.

--- client/src/Project/components/TestCreatorSection/ExceptionTab/ExceptionTab.jsx
+++ client/src/Project/components/TestCreatorSection/ExceptionTab/ExceptionTab.jsx
@@ -1,170 +1,176 @@
 import React, {useState, useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
-import {selectActiveTest, selectUnsavedActiveTest, updateException} from "../../../../reducers/activeTestInfoSlice";
+import {selectUnsavedActiveTest, updateException} from "../../../../reducers/activeTestInfoSlice";
+import ShouldEqualSelector from "../../../../shared/components/ShouldEqualSelector";
 import './ExceptionTab.scss';
-import {isEmpty} from "lodash";
+import {isEmpty, cloneDeep} from "lodash";
 
 function ExceptionTab({}) {
 
     const dispatch = useDispatch();
     const unsavedTest = useSelector(selectUnsavedActiveTest);
-    const activeTest = useSelector(selectActiveTest);
-
-    const [exceptionName, setExceptionName] = useState(
-        (isEmpty(activeTest))?
-            unsavedTest.moduleData.exception.value:activeTest.moduleData.exception.value
-    );
-    const [exceptionMessage, setExceptionMessage] = useState(
-        (isEmpty(activeTest))?
-        unsavedTest.moduleData.exception.message:activeTest.moduleData.exception.message
-    );
-
-    const [exceptionNameCheck, setExceptionNameCheck] = useState(exceptionName !== '');
-    const [exceptionMessageCheck, setExceptionMessageCheck] = useState(exceptionMessage !== '');
 
     const [exceptionNameErrorMessage, setExceptionNameErrorMessage] = useState('');
     const [exceptionMessageErrorMessage, setExceptionMessageErrorMessage] = useState('');
 
+    //const [exceptionNameCheck, setExceptionNameCheck] = useState(undefined);
+    //const [exceptionMessageCheck, setExceptionMessageCheck] = useState(undefined);
+
+    function onChangeCallback(exception) {
+        dispatch(updateException(exception));
+    }
+
     useEffect(() => {
-        if (!isEmpty(activeTest)) {
-            setExceptionName(activeTest.moduleData.exception.value);
-            setExceptionNameCheck(activeTest.moduleData.exception.value !== '');
-            setExceptionMessage(activeTest.moduleData.exception.message);
-            setExceptionMessageCheck(activeTest.moduleData.exception.message !== '');
+        if (unsavedTest.moduleData.exception.value !== undefined) {
+            if (validExceptionName(unsavedTest.moduleData.exception.value)) {
+                setExceptionNameErrorMessage('');
+            }
         }
-    }, [activeTest])
 
-    useEffect(() => {
+        if (unsavedTest.moduleData.exception.message !== undefined) {
+            if (ValidExceptionMessage(unsavedTest.moduleData.exception.message)) {
+                setExceptionMessageErrorMessage('');
+            }
+        }
+    }, [unsavedTest.moduleData.exception])
 
-        if (validExceptionName(exceptionName))
-            setExceptionNameErrorMessage('');
-
-        if (ValidExceptionMessage(exceptionMessage))
-            setExceptionMessageErrorMessage('');
-
-        dispatch(updateException({
-            name: exceptionName,
-            message: exceptionMessage
-        }));
-    }, [exceptionName, exceptionMessage]);
-
-    useEffect(() => {
-        if (!exceptionNameCheck)
-            setExceptionName('');
-        else
-            if ((!isEmpty(activeTest)) && (activeTest.moduleData.exception.value !== ''))
-                setExceptionName(activeTest.moduleData.exception.value);
-    }, [exceptionNameCheck])
-
-    useEffect(() => {
-        if (!exceptionMessageCheck)
-            setExceptionMessage('');
-        else
-        if ((!isEmpty(activeTest)) && (activeTest.moduleData.exception.message !== ''))
-            setExceptionMessage(activeTest.moduleData.exception.message);
-    }, [exceptionMessageCheck])
 
     function validExceptionName(exceptionName) {
-        if (exceptionNameCheck) {
-            if (exceptionName === '') {
-                setExceptionNameErrorMessage("Exception name can't be empty, either disable it or provide a name.");
-                return false;
-            }
-
-            const validName = exceptionName.match(/(^\d+|[^\w$]+)/g);
-            if (validName !== null) {
-                setExceptionNameErrorMessage('Exception name contains illegal characters');
-                return false;
-            }
+        if (exceptionName === '') {
+            setExceptionNameErrorMessage("Exception name can't be empty, either disable it or provide a name.");
+            return false;
+        }
+        const validName = exceptionName.match(/(^\d+|[^\w$]+)/g);
+        if (validName !== null) {
+            setExceptionNameErrorMessage('Exception name contains illegal characters');
+            return false;
         }
         return true;
     }
 
     function ValidExceptionMessage(exceptionMessage) {
-        if (exceptionMessageCheck) {
+        //if (exceptionMessageCheck) {
             if (exceptionMessage === '') {
                 setExceptionMessageErrorMessage("Exception message can't be empty, either disable it or provide a message.")
                 return false;
             }
-        }
+        //}
         return true;
     }
 
-    function onExceptionNameCallback(e) {
+    function onExceptionNameChangeCallback(e) {
         e.preventDefault();
-        setExceptionName(e.target.value);
+        const exceptionClone = cloneDeep(unsavedTest.moduleData.exception);
+        exceptionClone.value = e.target.value;
+        onChangeCallback(exceptionClone);
     }
 
-    function onExceptionMessageCallback(e) {
+    function onExceptionMessageChangeCallback(e) {
         e.preventDefault();
-        setExceptionMessage(e.target.value);
+        const exceptionClone = cloneDeep(unsavedTest.moduleData.exception);
+        exceptionClone.message = e.target.value;
+        onChangeCallback(exceptionClone);
+    }
+
+    function onExceptionEqualChangeCallback(e) {
+        e.preventDefault();
+        const exceptionClone = cloneDeep(unsavedTest.moduleData.exception);
+        exceptionClone.equal = (e.target.value === 'true');
+        onChangeCallback(exceptionClone);
+    }
+
+    function onCheckExceptionNameCallback(e) {
+        e.preventDefault();
+        const exceptionClone = cloneDeep(unsavedTest.moduleData.exception);
+        if (exceptionClone.value !== undefined) {
+            delete exceptionClone.value;
+            setExceptionNameErrorMessage('');
+        } else {
+            exceptionClone.value = '';
+        }
+        onChangeCallback(exceptionClone);
+    }
+
+    function onCheckExceptionMessageCallback(e) {
+        e.preventDefault();
+        const exceptionClone = cloneDeep(unsavedTest.moduleData.exception);
+        if (exceptionClone.message !== undefined) {
+            delete exceptionClone.message;
+            setExceptionMessageErrorMessage('');
+        } else {
+            exceptionClone.message = '';
+        }
+        onChangeCallback(exceptionClone);
+    }
+
+    if (isEmpty(unsavedTest)) {
+        return <div className="test-creator-tab-empty">waiting...</div>
     }
 
     return (
-      <div className="exception-tab-wrapper h-100">
-          <form
-            onSubmit={e => e.preventDefault()}
-            className="h-100"
-          >
-              <div className ="input-group exception-tab-exception-name">
-                  <div className="input-group-text">
-                      <input
-                          key ={'exception-tab-exception-name' + exceptionNameCheck}
-                          className="form-check-input"
-                          type="checkbox"
-                          checked={exceptionNameCheck}
-                          onChange={e => {
-                              e.preventDefault();
-                              setExceptionNameErrorMessage('');
-                              setExceptionNameCheck(!exceptionNameCheck);
-                          }}
-                      />
-                  </div>
-                  <span className="input-group-text">Name</span>
-                  <input
-                      type="text"
-                      className="form-control"
-                      value={exceptionName}
-                      onChange={onExceptionNameCallback}
-                      disabled={!exceptionNameCheck}
-                  />
-              </div>
-              <div className ="input-group exception-tab-exception-message">
-                  <div className="input-group-text">
-                      <input
-                          key ={'exception-tab-exception-message' + exceptionMessageCheck}
-                          className="form-check-input"
-                          type="checkbox"
-                          checked={exceptionMessageCheck}
-                          onChange={e => {
-                              e.preventDefault();
-                              setExceptionMessageErrorMessage('');
-                              setExceptionMessageCheck(!exceptionMessageCheck);
-                          }}
-                      />
+        <div className="exception-tab-wrapper">
+            <form
+                className ="h-100"
+                onSubmit={e => e.preventDefault()}
+            >
+                <ShouldEqualSelector
+                    value={unsavedTest.moduleData.exception.equal}
+                    onChange={onExceptionEqualChangeCallback}
+                    shouldEqualLabel={'Should Be Thrown'}
+                    shouldNotEqualLabel={'Should Not Be Thrown'}
+                />
+                <div className ="input-group exception-tab-exception-name">
 
-                  </div>
-                  <span className="input-group-text">Message</span>
-                  <input
-                      type="text"
-                      className="form-control"
-                      value={exceptionMessage}
-                      onChange={onExceptionMessageCallback}
-                      disabled={!exceptionMessageCheck}
-                  />
-              </div>
-              {(exceptionNameErrorMessage !== '') && (
-                  <div className ="alert alert-danger exception-tab-exception-name-alert">
-                      {exceptionNameErrorMessage}
-                  </div>
-              )}
-              {(exceptionMessageErrorMessage !== '') && (
-                  <div className ="alert alert-danger exception-tab-exception-message-alert">
-                      {exceptionMessageErrorMessage}
-                  </div>
-              )}
-          </form>
-      </div>
+                    <div className="input-group-text">
+                        <input
+                            key ={'exception-tab-exception-name' + (unsavedTest.moduleData.exception.value !== undefined)}
+                            className="form-check-input"
+                            type="checkbox"
+                            checked={(unsavedTest.moduleData.exception.value !== undefined)}
+                            onChange={onCheckExceptionNameCallback}
+                        />
+                    </div>
+                        <span className="input-group-text">Name</span>
+                        <input
+                            type="text"
+                            className="form-control"
+                            value={(unsavedTest.moduleData.exception.value) || ''}
+                            onChange={onExceptionNameChangeCallback}
+                            disabled={(unsavedTest.moduleData.exception.value === undefined)}
+                        />
+
+                </div>
+                {(exceptionNameErrorMessage !== '') && (
+                    <div className ="alert alert-danger exception-tab-exception-name-alert">
+                        {exceptionNameErrorMessage}
+                    </div>
+                )}
+                <div className ="input-group exception-tab-exception-message">
+                    <div className="input-group-text">
+                        <input
+                            key ={'exception-tab-exception-name' + (unsavedTest.moduleData.exception.message !== undefined)}
+                            className="form-check-input"
+                            type="checkbox"
+                            checked={(unsavedTest.moduleData.exception.message !== undefined)}
+                            onChange={onCheckExceptionMessageCallback}
+                        />
+                    </div>
+                    <span className="input-group-text">Message</span>
+                    <input
+                        type="text"
+                        className="form-control"
+                        value={(unsavedTest.moduleData.exception.message) || ''}
+                        onChange={onExceptionMessageChangeCallback}
+                        disabled={(unsavedTest.moduleData.exception.message === undefined)}
+                    />
+                </div>
+                {(exceptionMessageErrorMessage !== '') && (
+                    <div className ="alert alert-danger exception-tab-exception-message-alert">
+                        {exceptionMessageErrorMessage}
+                    </div>
+                )}
+            </form>
+        </div>
     );
 }
 

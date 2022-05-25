331dd31e38269448bb7d4dbf3e00c81d8ea882e1
Mårten Björkman <marten.bjorkman@apelkado.com>
2022-05-25 17:24:15 +0200

    Now the main instance of the database uses environment variables for the configuration, but will use the old settings if these variables are not set.

--- api/instances/database_main.py
+++ api/instances/database_main.py
@@ -1,9 +1,13 @@
 from api.database import *
 import sys
+import os
+
+MONGO_SERVER = os.environ.get('MONGO_SERVER', '127.0.0.1').upper()
+MONGO_PORT = os.environ.get('MONGO_PORT', '27017').upper()
 
 if __name__ != '__main__':
     try:
-        database_handler = DatabaseHandler('mongodb://127.0.0.1:27017')
+        database_handler = DatabaseHandler(f'mongodb://{MONGO_SERVER}:{MONGO_PORT}')
         database_handler.connect_to_db()
     except RuntimeError:
         pass
